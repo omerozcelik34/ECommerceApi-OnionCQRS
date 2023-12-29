9b00811501ca3064f959930337492aa3296d85db
Seyit Ömer ÖZÇELİK <seyitomerozcelik@hotmail.com>
2023-12-30 02:44:45 +0300

    temel entityler oluşturuldu.

---
+++ Core/ECommerceApi.Domain/Entities/Brand.cs
@@ -0,0 +1,22 @@
+﻿using ECommerceApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Domain.Entities
+{
+    public class Brand : EntityBase
+    {
+        public Brand()
+        {
+            
+        }
+        public Brand(string name)
+        {
+            Name = name;
+        }
+        public required string Name { get; set; }
+    }
+}
