aa96dd1e513ea1986ee57a30977503fbe9e280d5
Seyit Ömer ÖZÇELİK <seyitomerozcelik@hotmail.com>
2024-01-28 18:00:06 +0300

    UnitOfWork yapısı kuruldu.

---
+++ Core/ECommerceApi.Application/Interfaces/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using ECommerceApi.Application.Interfaces.Repositories;
+using ECommerceApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Application.Interfaces.UnitOfWorks
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IReadRepository<T> GetReadRepository<T>() where T : class, IEntityBase, new();
+        IWriteRepository<T> GetWriteRepository<T>() where T : class, IEntityBase, new();
+        Task<int> SaveAsync();
+        int Save();
+    }
+}
