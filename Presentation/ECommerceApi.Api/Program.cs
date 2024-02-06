4ae6a313e21c368d9abe37b11620fe114e622a31
Seyit Ömer ÖZÇELİK <seyitomerozcelik@hotmail.com>
2024-02-06 16:05:05 +0300

    Token service yapıldı, createToken metodu yapılandırıldı.

--- Presentation/ECommerceApi.Api/Program.cs
+++ Presentation/ECommerceApi.Api/Program.cs
@@ -1,5 +1,6 @@
 using ECommerceApi.Persistence;
 using ECommerceApi.Application;
+using ECommerceApi.Infrastructure;
 using ECommerceApi.Mapper;
 using ECommerceApi.Application.Exceptions;
 
@@ -20,6 +21,7 @@ builder.Configuration
     .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional : true);
 
 builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddCustomMapper();
 
