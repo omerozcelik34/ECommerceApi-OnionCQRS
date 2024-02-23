44f7c21706b855eae2efac92834d851b985a15c6
Seyit Ömer ÖZÇELİK <seyitomerozcelik@hotmail.com>
2024-02-23 12:57:41 +0300

    Login işlemleri tamamlandı.

--- Presentation/ECommerceApi.Api/Controllers/AuthController.cs
+++ Presentation/ECommerceApi.Api/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using ECommerceApi.Application.Features.Auth.Command.Register;
+﻿using ECommerceApi.Application.Features.Auth.Command.Login;
+using ECommerceApi.Application.Features.Auth.Command.Register;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -21,6 +22,13 @@ namespace ECommerceApi.Api.Controllers
         {
             await mediator.Send(request);
             return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginCommandRequest request)
+        {
+            var response = await mediator.Send(request);
+            return StatusCode(StatusCodes.Status200OK, response);
         }
     }
 }
