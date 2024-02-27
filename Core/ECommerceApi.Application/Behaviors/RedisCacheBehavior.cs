7b92de580e77799da6ac1eebed63b2a34e829338
Seyit Ömer ÖZÇELİK <seyitomerozcelik@hotmail.com>
2024-02-27 18:04:43 +0300

    Redis Connection, RedisService, RedisPipelineBehavior yapılanmaları tamamlandı

---
+++ Core/ECommerceApi.Application/Behaviors/RedisCacheBehavior.cs
@@ -0,0 +1,41 @@
+﻿using ECommerceApi.Application.Interfaces.RedisCache;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Application.Behaviors
+{
+    public class RedisCacheBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IRedisCacheService redisCacheService;
+
+        public RedisCacheBehavior(IRedisCacheService redisCacheService)
+        {
+            this.redisCacheService = redisCacheService;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if(request is ICacheableQuery query)
+            {
+                var cacheKey = query.CacheKey;
+                var cacheTime = query.CacheTime;
+
+                var cacheData = await redisCacheService.GetAsync<TResponse>(cacheKey);
+                if (cacheData is not null)
+                    return cacheData;
+
+                var response = await next();
+                if (response is not null)
+                    await redisCacheService.SetAsync(cacheKey, response, DateTime.Now.AddMinutes(cacheTime));
+
+                return response;
+            }
+
+            return await next();
+        }
+    }
+}
