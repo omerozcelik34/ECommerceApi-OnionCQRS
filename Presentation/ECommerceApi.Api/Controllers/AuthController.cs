16daaa9325b402dd02e081bcec98f93dd2303e84
Seyit Ömer ÖZÇELİK <seyitomerozcelik@hotmail.com>
2024-02-27 11:06:54 +0300

    Revoke işlemleri tamamlandı.

--- Presentation/ECommerceApi.Api/Controllers/AuthController.cs
+++ Presentation/ECommerceApi.Api/Controllers/AuthController.cs
@@ -1,6 +1,8 @@
 ﻿using ECommerceApi.Application.Features.Auth.Command.Login;
 using ECommerceApi.Application.Features.Auth.Command.RefreshToken;
 using ECommerceApi.Application.Features.Auth.Command.Register;
+using ECommerceApi.Application.Features.Auth.Command.Revoke;
+using ECommerceApi.Application.Features.Auth.Command.RevokeAll;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -37,6 +39,20 @@ namespace ECommerceApi.Api.Controllers
         {
             var response = await mediator.Send(request);
             return StatusCode(StatusCodes.Status200OK, response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Revoke(RevokeCommandRequest request)
+        {
+            await mediator.Send(request);
+            return StatusCode(StatusCodes.Status200OK);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RevokeAll()
+        {
+            await mediator.Send(new RevokeAllCommandRequest());
+            return StatusCode(StatusCodes.Status200OK);
         }
     }
 }
