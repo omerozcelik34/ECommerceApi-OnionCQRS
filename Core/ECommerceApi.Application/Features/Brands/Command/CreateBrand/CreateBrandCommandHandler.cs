b35bff0029fef52a8a4506bc318d1bb60ffec382
Seyit Ömer ÖZÇELİK <seyitomerozcelik@hotmail.com>
2024-02-28 17:47:19 +0300

    Redis test. Brand feature ve controller oluşturuldu. 1m veride test edildi.

---
+++ Core/ECommerceApi.Application/Features/Brands/Command/CreateBrand/CreateBrandCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using Bogus;
+using ECommerceApi.Application.Bases;
+using ECommerceApi.Application.Interfaces.AutoMapper;
+using ECommerceApi.Application.Interfaces.UnitOfWorks;
+using ECommerceApi.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Application.Features.Brands.Command.CreateBrand
+{
+    public class CreateBrandCommandHandler : BaseHandler, IRequestHandler<CreateBrandCommandRequest, Unit>
+    {
+        public CreateBrandCommandHandler(IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor) : base(mapper, unitOfWork, httpContextAccessor)
+        {
+        }
+
+        public async Task<Unit> Handle(CreateBrandCommandRequest request, CancellationToken cancellationToken)
+        {
+            Faker faker = new("tr");
+            List<Brand> brands = new();
+
+            for(int i = 0; i < 1000000; i++)
+            {
+                brands.Add(new(faker.Commerce.Department(1)));
+            }
+            
+            await unitOfWork.GetWriteRepository<Brand>().AddRangeAsync(brands);
+            await unitOfWork.SaveAsync();
+
+            return Unit.Value;
+        }
+    }
+}
